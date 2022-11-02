a13e523e6bb5c04f218e44db540d5b1cea4e39e3
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-11-01 20:09:04 -0400

    added static method to get opposite attribute

--- src/com/company/ATTRIBUTE_EFFECT.java
+++ src/com/company/ATTRIBUTE_EFFECT.java
@@ -1,9 +1,9 @@
 package com.company;
 
 public enum ATTRIBUTE_EFFECT {
-    POSITIVE (1.05),
-    NEGATIVE (0.95),
-    RANDOM (1.00);
+    POSITIVE (0.05),
+    NEGATIVE (-0.05),
+    RANDOM (0.00);
 
     private final double modifier;
 
@@ -13,5 +13,19 @@ public enum ATTRIBUTE_EFFECT {
 
     public double getModifier() {
         return this.modifier;
+    }
+
+    public static ATTRIBUTE_EFFECT getOpposite(ATTRIBUTE_EFFECT effect) {
+        switch (effect) {
+            case POSITIVE -> {
+                return NEGATIVE;
+            }
+            case NEGATIVE -> {
+                return POSITIVE;
+            }
+            default -> {
+                return null;
+            }
+        }
     }
 }
