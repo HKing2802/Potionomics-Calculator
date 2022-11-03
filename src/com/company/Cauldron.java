fd797f83320329284ff54b18d62ec809c976f6c7
amc7082 <amc7082@g.rit.edu>
2022-11-03 17:29:40 -0400

    Changed hasRandomEffect to getNumRandomEffect Added Comments

--- src/com/company/Cauldron.java
+++ src/com/company/Cauldron.java
@@ -3,16 +3,26 @@ package com.company;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Class representing a place to brew a potion, implementing ingredient and magimin caps.
+ *
+ * @author HKing
+ */
 public class Cauldron {
     private final int ingredientCap;
-    private final int maximCap;
+    private final int magiminCap;
     private final ATTRIBUTE_EFFECT[] attributes;
 
     private final List<Ingredient> ingredients;
 
-    public Cauldron(int ingredientCap, int maximCap) {
+    /**
+     * Constructor
+     * @param ingredientCap Capacity for ingredients
+     * @param magiminCap Capacity for total number of magimins across all ingredients in the cauldron
+     */
+    public Cauldron(int ingredientCap, int magiminCap) {
         this.ingredientCap = ingredientCap;
-        this.maximCap = maximCap;
+        this.magiminCap = magiminCap;
         this.ingredients = new LinkedList<>();
         this.attributes = new ATTRIBUTE_EFFECT[Ingredient.ATTRIBUTE_SLOTS];
     }
@@ -21,8 +31,8 @@ public class Cauldron {
         return this.ingredientCap;
     }
 
-    public int getMaximCap() {
-        return this.maximCap;
+    public int getMagiminCap() {
+        return this.magiminCap;
     }
 
     public List<Ingredient> getIngredients() {
@@ -33,7 +43,11 @@ public class Cauldron {
         return this.attributes;
     }
 
-    public int getMaximCount() {
+    /**
+     * Gets the total number of magimins in the ingredients in the cauldron
+     * @return number of magimins
+     */
+    public int getMagiminCount() {
         int count = 0;
 
         for (Ingredient ingredient : this.ingredients) {
@@ -43,27 +57,43 @@ public class Cauldron {
         return count;
     }
 
+    /**
+     * Checks if an ingredient can be added to the cauldron
+     * @param ingredient The ingredient to check
+     * @return boolean if the ingredient can be added
+     */
     public boolean canAddIngredient(Ingredient ingredient) {
-        return ((ingredient.getMagiminsCount() + this.getMaximCount()) < this.maximCap) && this.ingredients.size() < this.ingredientCap;
+        return ((ingredient.getMagiminsCount() + this.getMagiminCount()) <= this.magiminCap) && this.ingredients.size() < this.ingredientCap;
     }
 
+    /**
+     * Adds an ingredient to the cauldron
+     * @param ingredient The ingredient to check
+     * @throws IllegalArgumentException if ingredient can't fit
+     */
     public void addIngredient(Ingredient ingredient) {
-        if (this.canAddIngredient(ingredient)) {
-            this.ingredients.add(ingredient);
+        if (!this.canAddIngredient(ingredient)) {
+            throw new IllegalArgumentException("Ingredient cannot fit in Cauldron.");
+        }
+        this.ingredients.add(ingredient);
 
-            ATTRIBUTE_EFFECT[] newAttributes = ingredient.getAttributes();
-            for (int i = 0; i < this.attributes.length; i++) {
-                if (newAttributes[i] != null && this.attributes[i] != ATTRIBUTE_EFFECT.RANDOM) {
-                    if (this.attributes[i] == null) {
-                        this.attributes[i] = newAttributes[i];
-                    } else if (ATTRIBUTE_EFFECT.getOpposite(this.attributes[i]) == newAttributes[i]) {
-                        this.attributes[i] = ATTRIBUTE_EFFECT.RANDOM;
-                    }
+        ATTRIBUTE_EFFECT[] newAttributes = ingredient.getAttributes();
+        for (int i = 0; i < this.attributes.length; i++) {
+            if (newAttributes[i] != null && this.attributes[i] != ATTRIBUTE_EFFECT.RANDOM) {
+                if (this.attributes[i] == null) {
+                    this.attributes[i] = newAttributes[i];
+                } else if (ATTRIBUTE_EFFECT.getOpposite(this.attributes[i]) == newAttributes[i]) {
+                    this.attributes[i] = ATTRIBUTE_EFFECT.RANDOM;
                 }
             }
         }
     }
 
+    /**
+     * Gets a decimal modifier for the price based on the attributes provided by ingredients
+     * Random attributes do not apply to this modifier, and must be accounted for separately.
+     * @return decimal modifier for price
+     */
     public double getAttrModifier() {
         double mod = 1;
 
@@ -76,13 +106,19 @@ public class Cauldron {
         return Math.round(mod * 100.0) / 100.0;
     }
 
-    public boolean hasRandomEffect() {
+    /**
+     * Gets the number of attributes that are random
+     * @return number of random effect attributes
+     */
+    public int getNumRandomEffect() {
+        int count = 0;
+
         for (ATTRIBUTE_EFFECT effect : this.attributes) {
             if (effect == ATTRIBUTE_EFFECT.RANDOM) {
-                return true;
+                count++;
             }
         }
 
-        return false;
+        return count;
     }
 }
